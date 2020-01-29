6bb1d567fd70e15174cbb1707ee9db6281061c11
Gorka López-Quintana <>
2020-01-29 13:53:21 +0100

    Configuramos los parámetros iniciales para iniciar el servidor.

--- src/main/java/com/instancia2/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/instancia2/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.instancia2.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
@@ -10,5 +12,4 @@ public class EurekaServerApplication {
 
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
